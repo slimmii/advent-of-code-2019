024154b2bf2499d9e31244b347dd3c8b728a61a1
Andie Similon <andie@pickmeup.be>
2019-12-09 14:07:57 +0100

    Day 1 complete

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,20 +1,27 @@
 import fs from 'fs';
 import * as assert from 'assert';
 
-const process = (mass : number) => {
-    return Math.floor((mass / 3))-2;
-}
+const calculateFuelBase = (mass : number) => Math.floor((mass / 3))-2;
+const calculateTotalFuel = (mass : number) : number => {
+    const fuel = calculateFuelBase(mass);
+    console.log(fuel);
+    const additionalFuel = calculateFuelBase(fuel);
+    if (additionalFuel > 0) {
+        return fuel + calculateTotalFuel(fuel);
+    } else {
+        return fuel;
+    }
+};
 
-assert.equal(process(12), 2);
-assert.equal(process(14), 2);
-assert.equal(process(1969), 654);
-assert.equal(process(100756), 33583);
+assert.equal(calculateTotalFuel(14), 2);
+assert.equal(calculateTotalFuel(1969), 966);
+assert.equal(calculateTotalFuel(100756), 50346);
 
 const contents = fs.readFileSync('input', 'utf8');
 const input : number[] = contents.split('\n')
-                         .map(i => parseInt(i))
-                         .filter(i => Number.isInteger(i))
-                         .map(mass => process(mass));
+    .map(i => parseInt(i))
+    .filter(i => Number.isInteger(i))
+    .map(mass => calculateTotalFuel(mass));
 input.reduce(((previousValue, currentValue) => previousValue + currentValue));
 
 console.log(input.reduce(((previousValue, currentValue) => previousValue + currentValue)));
